d0c5cc9e8daad65e0daaec0afedb1e0af3e78f93
thoriqmacto <thariq.hadad@gmail.com>
2024-07-14 20:20:44 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,9 @@
 <?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',function (){
+    return response()->json([
+        'message' => 'Welcome to API'
+    ],200);
+});
