e31b1ca30facfbaf14602c04ee5587f66522d140
thoriqmacto <thariq.hadad@gmail.com>
2024-07-15 09:18:03 +0700

    designing the url

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login',[AuthController::class,'login']);
 Route::post('/register',[AuthController::class,'register']);
+
+Route::get('/tickets',function(){
+    return Ticket::all();
+});
