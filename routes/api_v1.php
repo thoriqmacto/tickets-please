ff332118c5f9a23c88a84002d5835b970b49fb51
thoriqmacto <thariq.hadad@gmail.com>
2024-07-16 11:32:37 +0700

    token authentication

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,9 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\AuthController;
-use App\Models\Ticket;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
